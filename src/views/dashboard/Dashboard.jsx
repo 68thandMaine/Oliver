899373627ea4548221afb12ea31b522520fbf417
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-12-31 12:42:47 -0800

    update grid styling

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -14,25 +14,35 @@ function Dashboard(props) {
 
   return (
 
-    <div>
-      {/* <div className='requirements'>
-        <h1>Dashboard Requirements</h1>
-        <p>This is the dasboard view. It should have the following components:</p>
-        <ul>
-          <li>Income This Month</li>
-          <li>Total amount of money available</li>
-          <li>Available Credit</li>
-          <li>Available Debit</li>
-          <li>Total Savings</li>
-          <li>Menu</li>
-          <li>Form Options</li>
-        </ul>
-      </div> */}
-
-      <Form 
-      formName='newLoan'/>
-      <button onClick={()=>console.log(state)}>Click</button>
-    </div>
+    <div className='container--fluid'>
+      <div className='container__row'>
+      <div className='container__col-lg-6'>
+        <div>
+          <h1>Dashboard Requirements</h1>
+          <p>This is the dasboard view. It should have the following components:</p>
+          <ul>
+            <li>Income This Month</li>
+            <li>Total amount of money available</li>
+            <li>Available Credit</li>
+            <li>Available Debit</li>
+            <li>Total Savings</li>
+            <li>Menu</li>
+            <li>Form Options</li>
+          </ul>
+        </div>
+      </div>
+      <div className='container__col-lg-6'>
+        <div className='container__row'>
+          <h1>Section 2</h1>
+          <p>This section will hold my graphs.</p> 
+        </div>
+        <div className='container__row'>
+          <h1>Section 3</h1>
+          <p>This section will be for controlling the graphs</p>
+        </div>
+        </div>
+      </div>
+  </div>
 
   );
 }
