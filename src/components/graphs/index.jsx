dfc0e60584c72eee84b6b7bebddb73ea0796944f
Chris Rudnicky <68thandMaine@gmail.com>
2020-02-26 22:51:39 -0800

    Trim excess divs

--- src/components/graphs/index.jsx
+++ src/components/graphs/index.jsx
@@ -2,24 +2,12 @@ import React from 'react';
 import rd3 from 'react-d3-library';
 
 function Graphs(props) {
-//  console.log(props.graphData)
-
-
-
-// buildGraph() {
-  // const data = {};
-
-  // data.width = 500;
-  // data.height = 750;  
-  // data.dataset =  Object.entries(props.graphData) => { time:  }
-  // }
-
 
   return (
-    <div>
-      <h1>It's me the graphs component!</h1>
+    <div classname="container-fluid">
+      <h1>Graph Title</h1>
     </div>
-  );
+      );
 };
 
 export default Graphs;
