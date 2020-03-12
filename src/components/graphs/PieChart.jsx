9283084d751ffeeee1eb89546f92af47cd3acb3a
Chris Rudnicky <68thandMaine@gmail.com>
2020-03-11 19:30:58 -0700

    Donut chart added

--- src/components/graphs/PieChart.jsx
+++ src/components/graphs/PieChart.jsx
@@ -2,22 +2,45 @@ import React, { useEffect, useState } from 'react';
 import * as d3 from 'd3';
 
 function PieChart() {
-
-  const data = [ 2, 4, 6, 8 ];
-  const [ chartData, setChartData ] = useState(data);
+  const width = 450;
+  const height = width;
+  const margin = 40;
+  const radius = Math.min(width, height) / 2 - margin
+  const data = {a: 9, b: 20, c:30, d:8, e:12}
+  const color = d3.scaleOrdinal()
+    .domain(data)
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
   const pie = d3.pie()
-  console.log(pie(chartData))
+      .value((d) => { return d.value; })
+  const data_ready = pie(d3.entries(data))
 
-  
   useEffect(() => {
-    // d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
-    // createPie()
+  // append the svg object to the div called 'my_dataviz'
+  const svg = d3.select("#donutChart")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+      .selectAll('whatever')
+      .data(data_ready)
+      .enter()
+      .append('path')
+      .attr('d', d3.arc()
+        .innerRadius(100)         // This is the size of the donut hole
+        .outerRadius(radius)
+      )
+      .attr('fill', function(d){ return(color(d.data.key)) })
+      .attr("stroke", "black")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7)
   });
 
   return (
-    <svg height="210" width="400">
-      <path d="M150 0 L75 200 L225 200 Z" />
-    </svg>
+    <div id='donutChart'></div>
   )
 }
 
--- src/components/graphs/index.jsx
+++ src/components/graphs/index.jsx
@@ -5,49 +5,14 @@ import Circles from './Circles';
 import PieChart from './PieChart';
 
 function Graphs(props) {
-  const exampleData = [ 8, 5, 13, 9, 12 ];
-  const [data, setData ] = useState(exampleData);
-
-  useEffect(() => {
-    drawBarChart(data);
-  })
-
-  function drawBarChart(data) {
-    const canvasHeight = 400;
-    const canvasWidth = 600;
-    const scale = 20;
-
-    const svgCanvas = d3.select("#canvas")
-      .append("svg")
-      .attr("width", canvasWidth)
-      .attr("height", canvasHeight)
-      .style("margin", 0)
-      .style("border", "1px solid white")
-
-      svgCanvas.selectAll("rect")
-      .data(data).enter()
-           .append("rect")
-           .attr("width", 40)
-           .attr("height", (datapoint) => datapoint * 20)
-           .attr("fill", "cyan")
-           .attr("x", (datapoint, iteration) => iteration * 45)
-           .attr("y", (datapoint) => canvasHeight - datapoint * scale)
-
-      svgCanvas.selectAll("text")
-      .data(data).enter()
-      .append("text")
-      .attr("x", (dataPoint, i) => i * 45 + 10)
-      .attr("y", (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
-      .text(dataPoint => dataPoint)
-      .style("color", "white")
-
-  }
-
+  
 
   return (
-    // <BarChart />
-    // <Circles />
-    <PieChart />
+    <div className = 'container'>
+      {/* <BarChart /> */}
+      {/* <Circles /> */}
+      <PieChart />
+    </div>
   )
 };
 
