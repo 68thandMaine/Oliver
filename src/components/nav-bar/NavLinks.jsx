10a11c7b0fe93c62540063dd84ebbf6bb37d707c
Chris Rudnicky <68thandMaine@gmail.com>
2020-04-03 09:39:38 -0700

    WIP - making navbar neumorphic

--- src/components/nav-bar/NavLinks.jsx
+++ src/components/nav-bar/NavLinks.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 function NavLinks(props) {
   const { assignActiveClass } = props
-  const links = props.linkList.map((link) => <li key={link.id} id={link.name} onClick={() => assignActiveClass(link.name)} className='navbar_link u-pl5 u-pt3 text-lg'>{link.name}</li>)
+  const links = props.linkList.map((link) => <li key={link.id} id={link.name} onClick={() => assignActiveClass(link.name)} className='navbar_link u-pl5 text-lg'>{link.name}</li>)
 
   return (
-    <ul className="navbar--horizontal navbar__link  u-ml8">
+    <ul className="navbar--horizontal navbar__link  u-ml7">
       {links}
     </ul>
   );
