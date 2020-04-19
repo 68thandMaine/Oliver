be0a2a94033a24bc0ecf7647cf09c8cca9c5d158
Christopher Rudnicky <crudnick@L-C02ZF7BELVDQ>
2020-04-18 20:15:20 -0700

    WIP

--- src/components/nav-bar/NavLinks.jsx
+++ src/components/nav-bar/NavLinks.jsx
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types';
 
 function NavLinks(props) {
   const { assignActiveClass } = props
-<<<<<<< HEAD
   const links = props.linkList.map((link) => <li key={link.id} id={link.name} onClick={() => assignActiveClass(link.name)} className='navbar_link u-pl5 text-lg'>{link.name}</li>)
-=======
-  const links = props.linkList.map((link) => <li key={link.id} id={link.name} onClick={() => assignActiveClass(link.name)} className='navbar_link u-pl5 u-pt3 text-lg'>{link.name}</li>)
->>>>>>> 25ff597... WIP
 
   return (
     <ul className="navbar--horizontal navbar__link  u-ml7">
