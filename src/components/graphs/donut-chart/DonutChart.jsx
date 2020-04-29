c462f325e67f98dcee00cbee4b47765fabb34e62
Christopher Rudnicky <crudnick@L-C02ZF7BELVDQ>
2020-04-28 23:38:53 -0700

    GRAPHS-donut-chart-with-label: add donut chart beginnings

---
+++ src/components/graphs/donut-chart/DonutChart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DonutChart() {
+// Donut chart needs to have data that is in the shape of an object.
+    return (
+        <section>
+            hi
+        </section>
+    )
+}
+
+export default DonutChart;
