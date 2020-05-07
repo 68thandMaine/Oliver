900ec60d814c12bab227baf35530d88464dd1442
Christopher Rudnicky <crudnick@L-C02ZF7BELVDQ>
2020-05-07 14:02:14 -0700

    GRAPHS-create_legend: add example legend component

--- src/components/graphs/line_graph/Legend.jsx
+++ src/components/graphs/line_graph/Legend.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { select, selectAll } from 'd3-selection';
+import { schemeSet2 } from 'd3-scale-chromatic';
 import PropTypes from 'prop-types';
+import { scaleOrdinal } from 'd3';
 
 class Legend extends React.Component {
   constructor(props) {
@@ -16,44 +18,40 @@ class Legend extends React.Component {
     //   legendItems: this.createLegendItems()
     // });
 
-   let colorLegend = ["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)"];
+   let color =  scaleOrdinal()
+    .domain(this.state.legendItems)
+    .range(schemeSet2);
 
    let legend = select('#legend');
 
-   // color 
-    legend.selectAll('rect')
-    .data(colorLegend)
-    .enter()
-    .append('rect')
-    .attr('fill', function(d, i) { return colorLegend[i]; })
-    .attr('x', function(d, i) { return (i*30); })
-    .attr('y', 30)
-    .attr('width', 30)
-    .attr('height', 20);
+   // legend item icon 
+    legend.selectAll('dots')
+      .data(this.state.legendItems)
+      .enter()
+      .append('circle')
+        .attr('cx', 100)
+        .attr('cy', ((d, i) => 100 + i*25))
+        .attr('r', 7)
+        .style('fill',(d) => color(d))
 
     // title
     legend.append('text')
-    // .attr('transform', 'translate(500, 525')
-    .attr('font-size', '12px')
-    .attr('font-family', 'HelveticaNeue-Bold, Helvetica, sans-serif')
-    .attr('fill','white')
-    .attr('y', 20)
-    .text('helloooo')
+      .attr('font-size', '12px')
+      .attr('font-family', 'HelveticaNeue-Bold, Helvetica, sans-serif')
+      .attr('fill','white')
+      .attr('y', 20)
+      .text('helloooo')
 
-    legend.append('g')
-      // .attr('transform', 'translate(500, 525)')
-      .selectAll('text')
+   // labels
+    legend.selectAll('labels')
       .data(this.state.legendItems)
       .enter()
       .append('text')
-      .attr('font-size', '10px')
-      .attr('font-family', 'HelveticaNeue-Bold, Helvetica, sans-serif')
-      .attr('fill', 'red')
-      .attr('x', function(d,i){return (i*30)})
-      .attr('y', 60)
-      .text(function (d) {return d})
-
-    console.log(select('#legend'))
+        .attr('x', 120)
+        .attr('y', (d,i) => 100 + i * 25 )
+        .style('fill', (d) => color(d))
+        .text((d => d))
+        .style('alignment-baseline', 'middle');
   }
     
   // Creates the state array for items in the legend
