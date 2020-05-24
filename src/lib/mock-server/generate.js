8d99bcc235e433eb4167a43260f45e49706a141c
Chris Rudnicky <chrisrudnicky@gmail.com>
2020-05-23 20:51:16 -0700

    DX-DATA-mock-data-generation: create fake bank accounts

--- src/lib/mock-server/generate.js
+++ src/lib/mock-server/generate.js
@@ -2,10 +2,23 @@ module.exports = function() {
 	const faker = require('faker');
 	const _ = require('lodash');
 	return {
-		transactions: _.times(100,(n) => {
+		bankAccounts: _.times((2),(n) => {
 			return {
 				id: n,
-				name: faker.name.findName(),
+				bankName: faker.company.companyName(),
+				// create a random number with nine digits
+				routingNumber: faker.random.number({
+					min: 100000000,
+					max: 999999999
+				}),
+				accountNumber: faker.random.number({
+					// create a random number with up to twelve digits
+					min: 1000000000,
+					max: 99999999999
+				}),
+				accountType: faker.finance.accountName(),
+				accountBalance: faker.finance.amount(),
+				createdAt: faker.date.past()
 			}
 		})
 	}
