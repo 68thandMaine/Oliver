cefb4d822ac5e02ca616b76986d9e7f0f8f342b4
Chris Rudnicky <chrisrudnicky@gmail.com>
2020-06-21 00:57:34 -0700

    ACCOUNTS: add sidebar

---
+++ src/components/navigation/sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const SideBar = (props) => {
+	return (
+		<aside>
+			<ul>
+				<li>Banking
+					<ul>
+						<li>New</li>
+					</ul>
+				</li>
+				<li>Credit Cards
+					<ul>
+						<li>New</li>
+					</ul>
+				</li>
+				<li>Loans
+					<ul>
+						<li>New</li>
+					</ul>
+				</li>
+			</ul>
+		</aside>
+	)
+}
+
+export default SideBar;
