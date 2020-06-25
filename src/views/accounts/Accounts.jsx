4d18cc9d8d9decac7b682ff3281118f61adde96d
Chris Rudnicky <chrisrudnicky@gmail.com>
2020-06-24 23:31:46 -0700

    GRAPH-graph-card: add skeleton of graph card
    Going to update it as I build more functionality

--- src/views/accounts/Accounts.jsx
+++ src/views/accounts/Accounts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Card from '../../components/card/Card';
+import GraphCard from '../../components/card/GraphCard';
 import Form from '../../components/forms';
 import Sidebar from '../../components/navigation/sidebar/Sidebar';
 
@@ -10,8 +10,7 @@ const Accounts = () => {
 		<section className="flex border border-red-500">
 			<Sidebar />
 			<section className="w-4/5 px-2">
-				<Form
-					formName="newCreditCard" />
+				<GraphCard />
 			</section>
 		</section>
 			
