6d5a5cbd2fbad9e3472d27220917bfab81af0ed2
Chris Rudnicky <chrisrudnicky@gmail.com>
2020-07-06 21:42:36 -0700

    ACCOUNTS: Add failure / success action types for bank accounts

--- src/__tests__/unit/action-creators/BankAccount.js
+++ src/__tests__/unit/action-creators/BankAccount.js
@@ -2,3 +2,6 @@ import {BankAccount} from '../../../actions/index';
 import constants from '../../../constants';
 const { action } = constants;
 
+describe('Bank account actions', () => {
+	it('Should ')
+});
